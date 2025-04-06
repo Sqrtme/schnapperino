cbdfb82615be54909fdccc63dcbe4275fe59a906
Filipp <Caps2073@gmail.com>
2025-04-06 22:44:02 +0200

    jpg

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,6 @@
 <template>
   <!-- <Main /> -->
   <div class="flex items-center justify-center h-[100vh]">
-    <img class="w-auto" src="/public/images/base.png" alt="Icon" />
+    <img class="w-auto" src="/public/images/base.jpg" alt="Icon" />
   </div>
 </template>
