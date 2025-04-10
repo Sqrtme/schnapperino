6bf07e881d808dba61479e614dd36131c008e718
Filipp <Caps2073@gmail.com>
2025-04-10 22:21:08 +0200

    main page upd; impresssum view upd; icons added

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="flex flex-col items-center mt-14">
-    <h1 class="mb-1"><b>Impresum</b></h1>
-    <h2>Schnapperino GmbH</h2>
-    <h2 class="mb-4">Die Adresse</h2> 
-    <h2 class="mb-1"><b>Geschäftsführer:</b></h2>
-    <h3>Vorname Name</h3>
-    <h3 class="mb-4">Vorname Name</h3>
-
-    <h2 class="mb-1"><b>Registrierung:</b></h2>
-    <h3 class="w-2/3 mb-4 indent-4 max-w-[800px]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex voluptatem iusto eveniet veniam, natus, cumque consequatur dolorem ipsa nemo laboriosam vero quam suscipit est atque assumenda repudiandae cum illo error.</h3>
-    <h2 class="mb-1"><b>Umsatzsteuer-Identifikationsnummer:</b></h2>
-    <h3 class="mb-4">USt-Nr. _______ USt. ID: ______</h3>
-    <h2 class="mb-1"><b>Kontakt:</b></h2>
-    <h3 class="mb-4">E-mail: info@schnapperino.de</h3>
-    <h2 class="mb-1"><b>Haftungshinweis</b></h2>
-    <h3 class="mb-4 w-2/3 indent-4 max-w-[800px]">Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.</h3>
-    <h2 class="mb-1"><b>Urheberrechtshinweis</b></h2>
-    <h3 class="mb-4 w-2/3 indent-4 max-w-[800px]">Die Inhalte und Werke auf dieser Website sind urheberrechtlich geschützt. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechts bedürfen der schriftlichen Zustimmung von Holy Sunshine Living GmbH oder des jeweiligen Urhebers bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</h3>
+    <div class="flex items-center justify-center flex-col py-6 rounded-xl bg-slate-50">
+        <NuxtLink to="/" class="w-full flex items-center pl-6">
+          <Icon size="24" name="flowbite:arrow-left-outline" style="color: black" />
+          <span class="text-sm font-bold">To main</span> 
+        </NuxtLink>
+      <h1 class="mb-1"><b>Impresum</b></h1>
+      <h2>Schnapperino GmbH</h2>
+      <h2 class="mb-4">Die Adresse</h2> 
+      <h2 class="mb-1"><b>Geschäftsführer:</b></h2>
+      <h3>Vorname Name</h3>
+      <h3 class="mb-4">Vorname Name</h3>
+      <h2 class="mb-1"><b>Registrierung:</b></h2>
+      <h3 class="w-2/3 mb-4 indent-4 max-w-[800px]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex voluptatem iusto eveniet veniam, natus, cumque consequatur dolorem ipsa nemo laboriosam vero quam suscipit est atque assumenda repudiandae cum illo error.</h3>
+      <h2 class="mb-1"><b>Umsatzsteuer-Identifikationsnummer:</b></h2>
+      <h3 class="mb-4">USt-Nr. _______ USt. ID: ______</h3>
+      <h2 class="mb-1"><b>Kontakt:</b></h2>
+      <h3 class="mb-4">E-mail: info@schnapperino.de</h3>
+      <h2 class="mb-1"><b>Haftungshinweis</b></h2>
+      <h3 class="mb-4 w-2/3 indent-4 max-w-[800px]">Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.</h3>
+      <h2 class="mb-1"><b>Urheberrechtshinweis</b></h2>
+      <h3 class="mb-4 w-2/3 indent-4 max-w-[800px]">Die Inhalte und Werke auf dieser Website sind urheberrechtlich geschützt. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechts bedürfen der schriftlichen Zustimmung von Holy Sunshine Living GmbH oder des jeweiligen Urhebers bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</h3>
+    </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,12 @@
 </script>
 
 <template>
-  <!-- <Main /> -->
-  <div class="flex items-center justify-center h-[100vh]">
+  <div class="flex items-center justify-start h-[100vh] flex-col">
     <img class="w-auto" src="/public/images/base.jpg" alt="Icon" />
+    <div class="flex gap-10 justify-center items-center text-xl font-bold">
+      <NuxtLink to="/locations"><p class="hover-underline">Locations</p></NuxtLink>
+      <NuxtLink to="/products"><p class="hover-underline">Products</p></NuxtLink>
+      <NuxtLink to="/impressum"><p class="hover-underline">Impressum</p></NuxtLink>
+    </div>
   </div>
 </template>
